c174a5c2dad9cd8e8b4f8081a340e9dc32a17b7c
Muhammad_Rahban <muhammadrahban4@gmail.com>
2023-11-23 00:17:09 -0800

    feat():migration and adoption from database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,25 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
+            $table->string('nationality')->nullable();
+            $table->string('address')->nullable();
+                        $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->string('post_code')->nullable();
+            $table->string('organiation')->nullable();
+            $table->string('job_title')->nullable();
+            $table->string('comments')->nullable();
+            $table->boolean('is_anonymously')->default(0);
+            $table->boolean('is_individual')->default(0);
+            $table->boolean('is_company')->default(0);
+
             $table->rememberToken();
             $table->timestamps();
         });
