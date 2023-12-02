33edc90767a94482e96e37f73137b3e6e89fd13a
wasifwaheed <wasifwaheedonline@gmail.com>
2023-12-02 12:57:36 +0500

    fix(): verify message design

--- resources/views/web/verify-message.blade.php
+++ resources/views/web/verify-message.blade.php
@@ -3,8 +3,10 @@
     <main>
         <section style="background-image: url({{ asset('images/shape-8.png') }}); background-size:auto 100%;">
             <div class="container py-5">
-                <!-- Your message displayed here -->
-                <p>{{ $data['message'] }}</p>
+                <div style="background-color: #f8f8f8; border-radius: 8px; padding: 20px; text-align: center; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); max-width: 500px; margin: 0 auto;">
+                    <h2 style="font-size: 24px; color: #333; margin-bottom: 10px;">Verify Your Email</h2>
+                    <p style="font-size: 18px; color: #555;">Please verify your email to adopt the victim.</p>
+                </div>
             </div>
         </section>
     </main>
