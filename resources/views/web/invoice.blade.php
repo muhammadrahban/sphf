9cd0248250aea206ace24d569669ff12afebb27e
wasifwaheed <wasifwaheedonline@gmail.com>
2023-12-02 15:44:56 +0500

    fix(): invoice view

--- resources/views/web/invoice.blade.php
+++ resources/views/web/invoice.blade.php
@@ -1,14 +1,7 @@
 @extends('web.master')
 @section('webcontain')
-
     <style>
         /* Styles for the invoice */
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 20px;
-        }
         .invoice {
             background-color: #fff;
             border-radius: 8px;
@@ -17,12 +10,15 @@
             max-width: 500px;
             margin: 0 auto;
         }
+
         h1 {
             color: #333;
         }
+
         p {
             margin-bottom: 10px;
         }
+
         .btn {
             display: inline-block;
             padding: 10px 20px;
@@ -31,33 +27,36 @@
             text-decoration: none;
             border-radius: 5px;
         }
+
         .btn:hover {
             background-color: #0056b3;
         }
     </style>
+    <section style="background-image: url({{ asset('images/shape-8.png') }}); background-size:auto 100%;">
+        <div class="container py-5">
 
+            <div class="invoice">
+                <h1>Invoice</h1>
 
-<div class="invoice">
-    <h1>Invoice</h1>
+                <!-- Use the provided JSON data -->
+                <p><strong>Transaction ID:</strong> {{ $decodedData['TransactionId'] }}</p>
+                <p><strong>Transaction Amount:</strong> {{ $decodedData['TransactionAmount'] }}</p>
+                <p><strong>Transaction Status:</strong> {{ $decodedData['TransactionStatus'] }}</p>
+                <p><strong>Order Date & Time:</strong> {{ $decodedData['OrderDateTime'] }}</p>
+                <p><strong>Transaction Date & Time:</strong> {{ $decodedData['TransactionDateTime'] }}</p>
+                <p><strong>Merchant ID:</strong> {{ $decodedData['MerchantId'] }}</p>
+                <p><strong>Merchant Name:</strong> {{ $decodedData['MerchantName'] }}</p>
+                <p><strong>Store ID:</strong> {{ $decodedData['StoreId'] }}</p>
+                <p><strong>Store Name:</strong> {{ $decodedData['StoreName'] }}</p>
+                <p><strong>Transaction Type ID:</strong> {{ $decodedData['TransactionTypeId'] }}</p>
+                <p><strong>Transaction Reference Number:</strong> {{ $decodedData['TransactionReferenceNumber'] }}</p>
+                <p><strong>Mobile Number:</strong> {{ $decodedData['MobileNumber'] }}</p>
+                <p><strong>Description:</strong> {{ $decodedData['Description'] }}</p>
+                <p><strong>Response Code:</strong> {{ $decodedData['ResponseCode'] }}</p>
 
-    <!-- Use the provided JSON data -->
-    <p><strong>Transaction ID:</strong> {{ $decodedData['TransactionId'] }}</p>
-    <p><strong>Transaction Amount:</strong> {{ $decodedData['TransactionAmount'] }}</p>
-    <p><strong>Transaction Status:</strong> {{ $decodedData['TransactionStatus'] }}</p>
-    <p><strong>Order Date & Time:</strong> {{ $decodedData['OrderDateTime'] }}</p>
-    <p><strong>Transaction Date & Time:</strong> {{ $decodedData['TransactionDateTime'] }}</p>
-    <p><strong>Merchant ID:</strong> {{ $decodedData['MerchantId'] }}</p>
-    <p><strong>Merchant Name:</strong> {{ $decodedData['MerchantName'] }}</p>
-    <p><strong>Store ID:</strong> {{ $decodedData['StoreId'] }}</p>
-    <p><strong>Store Name:</strong> {{ $decodedData['StoreName'] }}</p>
-    <p><strong>Transaction Type ID:</strong> {{ $decodedData['TransactionTypeId'] }}</p>
-    <p><strong>Transaction Reference Number:</strong> {{ $decodedData['TransactionReferenceNumber'] }}</p>
-    <p><strong>Mobile Number:</strong> {{ $decodedData['MobileNumber'] }}</p>
-    <p><strong>Description:</strong> {{ $decodedData['Description'] }}</p>
-    <p><strong>Response Code:</strong> {{ $decodedData['ResponseCode'] }}</p>
-
-    <!-- Add a download button for the invoice -->
-    <a href="{{ route('download.invoice') }}" class="btn">Download Invoice</a>
-</div>
-
+                <!-- Add a download button for the invoice -->
+                <a href="{{ route('download.invoice') }}" class="btn">Download Invoice</a>
+            </div>
+        </div>
+    </section>
 @endsection
